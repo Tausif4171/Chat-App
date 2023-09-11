0b0be9f4d93dffdf3a42913e198a8404bb05e6a9
Tausif4171 <tausifkhan4171@gmail.com>
2023-09-11 18:14:54 +0530

    tailwind css setup ✅

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hii</h1>
+    <div className='flex justify-center items-center w-full'>
+      <h1 className='font-bold text-[26px] text-[#35c741]'>Chat App!</h1>
     </div>
   );
 }
