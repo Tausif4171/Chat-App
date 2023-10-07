2e5580e5ce55cefb4edb3c0244d640ac2db2912f
Tausif4171 <tausifkhan4171@gmail.com>
2023-10-08 00:27:14 +0530

    single chat api test on dummy data ✅

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,4 +12,9 @@ app.get('/api/chats', (req, res) => {
     res.send(chats);
 });
 
+app.get('/api/chats/:id', (req, res) => {
+    const singleUserChat = chats.find((chat) => chat._id === req.params.id)
+    res.send(singleUserChat)
+})
+
 app.listen(5000, () => console.log('Server is running on port 5000'));
