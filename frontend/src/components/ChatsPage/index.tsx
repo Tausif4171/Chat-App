240df1a84722584068a1db989b83f34c0aa188c8
Tausif4171 <tausifkhan4171@gmail.com>
2024-01-01 18:57:53 +0530

    fetching chat data from backend

--- frontend/src/components/ChatsPage/index.tsx
+++ frontend/src/components/ChatsPage/index.tsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function ChatPage() {
+    const [chats, setChats] = useState<any>([])
+    console.log({ chats })
+    const fetchChats = async () => {
+        try {
+            const result = await axios.get('/api/chats');
+            setChats(result.data);  // Assuming the data is in the 'data' property of the response
+        } catch (error: any) {
+            console.error('Error fetching chats:', error.message);
+        }
+    }
+
+
+    useEffect(() => {
+        fetchChats()
+    }, [])
     return (
         <div>ChatPage</div>
     )
