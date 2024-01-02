c31abbf575e1e79669f4b770d1f9dc99bc28855d
Tausif4171 <tausifkhan4171@gmail.com>
2024-01-02 22:19:51 +0530

    login form ui

--- frontend/src/components/HomePage/index.tsx
+++ frontend/src/components/HomePage/index.tsx
@@ -1,14 +1,30 @@
-import { Box, Container } from '@chakra-ui/react'
+import { Box, Button, Container, FormControl, FormLabel, Input, Text } from '@chakra-ui/react'
 import React from 'react'
 
 function HomePage() {
     return (
         <>
             <div style={{ display: 'flex', justifyContent: 'start', alignItems: 'start' }}>
-                <Container maxW='2xl' bg='blue.600' centerContent>
-                    <Box padding='4' color='white' maxW='md'>
-                        Talkative
+                <Container maxW='2xl' bg='#F0F0F8' centerContent>
+                    <Box padding='4' color='black' maxW='md'>
+                        <Text fontSize='32px' fontWeight={600}>Talkative</Text>
                     </Box>
+
+                    <FormControl isRequired mb={'24px'}>
+                        <Box mb={'24px'}>
+                            <FormLabel>Username</FormLabel>
+                            <Input placeholder='First name' bg={'white'} />
+                        </Box>
+
+                        <Box mb={'24px'}>
+                            <FormLabel>Password</FormLabel>
+                            <Input placeholder='First name' bg={'white'} />
+                        </Box>
+
+                        <Box mb={'24px'}>
+                            <Button bg='black' width={'107px'} height={'44px'} textAlign={'center'} lineHeight={'20px'} rounded={'12px'} textColor={'white'}>Button</Button>
+                        </Box>
+                    </FormControl>
                 </Container>
             </div>
         </>
