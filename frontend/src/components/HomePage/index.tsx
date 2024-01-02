bc0839cad5d236a3e3bcdaaea2cf66b5d13af054
Tausif4171 <tausifkhan4171@gmail.com>
2024-01-02 22:03:32 +0530

    header added

--- frontend/src/components/HomePage/index.tsx
+++ frontend/src/components/HomePage/index.tsx
@@ -1,8 +1,17 @@
+import { Box, Container } from '@chakra-ui/react'
 import React from 'react'
 
 function HomePage() {
     return (
-        <div>HomePage</div>
+        <>
+            <div style={{ display: 'flex', justifyContent: 'start', alignItems: 'start' }}>
+                <Container maxW='2xl' bg='blue.600' centerContent>
+                    <Box padding='4' color='white' maxW='md'>
+                        Talkative
+                    </Box>
+                </Container>
+            </div>
+        </>
     )
 }
 
