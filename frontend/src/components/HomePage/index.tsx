9e05134fffcce23d986558b975c4802956ff8bb4
Tausif4171 <tausifkhan4171@gmail.com>
2024-01-03 23:07:25 +0530

    sign up fields added

--- frontend/src/components/HomePage/index.tsx
+++ frontend/src/components/HomePage/index.tsx
@@ -19,18 +19,18 @@ function HomePage() {
                             <TabPanel>
 
                                 <FormControl isRequired mb={'24px'}>
-                                    <Box mb={'24px'}>
-                                        <FormLabel>Username</FormLabel>
-                                        <Input placeholder='First name' bg={'white'} focusBorderColor='none' outline={'none'} />
+                                    <Box mb={'12px'}>
+                                        <FormLabel>Email Address</FormLabel>
+                                        <Input placeholder='Enter name' bg={'white'} focusBorderColor='none' outline={'none'} />
                                     </Box>
 
-                                    <Box mb={'24px'}>
+                                    <Box mb={'12px'}>
                                         <FormLabel>Password</FormLabel>
-                                        <Input placeholder='First name' bg={'white'} />
+                                        <Input placeholder='Enter password' bg={'white'} />
                                     </Box>
 
                                     <Box mb={'24px'}>
-                                        <Button bg='black' width={'107px'} height={'44px'} textAlign={'center'} lineHeight={'20px'} rounded={'12px'} textColor={'white'}>Button</Button>
+                                        <Button bg='black' width={'107px'} height={'44px'} textAlign={'center'} lineHeight={'20px'} rounded={'12px'} textColor={'white'}>Login</Button>
                                     </Box>
                                 </FormControl>
 
@@ -38,17 +38,25 @@ function HomePage() {
                             <TabPanel>
 
                                 <FormControl isRequired mb={'24px'}>
-                                    <Box mb={'24px'}>
-                                        <FormLabel>Username</FormLabel>
-                                        <Input placeholder='First name' bg={'white'} focusBorderColor='none' outline={'none'} />
+                                    <Box mb={'12px'}>
+                                        <FormLabel>Name</FormLabel>
+                                        <Input placeholder='Enter name' bg={'white'} focusBorderColor='none' outline={'none'} />
+                                    </Box>
+                                    <Box mb={'12px'}>
+                                        <FormLabel>Email</FormLabel>
+                                        <Input placeholder='Enter email' bg={'white'} focusBorderColor='none' outline={'none'} />
                                     </Box>
 
-                                    <Box mb={'24px'}>
+                                    <Box mb={'12px'}>
                                         <FormLabel>Password</FormLabel>
-                                        <Input placeholder='First name' bg={'white'} />
+                                        <Input placeholder='Enter password' bg={'white'} />
+                                    </Box>
+                                    <Box mb={'12px'}>
+                                        <FormLabel>Confirm Password</FormLabel>
+                                        <Input placeholder='Enter confirm password' bg={'white'} />
                                     </Box>
 
-                                    <Box mb={'24px'}>
+                                    <Box mb={'12px'}>
                                         <Button bg='black' width={'107px'} height={'44px'} textAlign={'center'} lineHeight={'20px'} rounded={'12px'} textColor={'white'}>Button</Button>
                                     </Box>
                                 </FormControl>
