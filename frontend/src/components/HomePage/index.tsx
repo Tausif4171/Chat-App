144c916754a29f30a84933c57669b28522bdf062
Tausif4171 <tausifkhan4171@gmail.com>
2024-01-03 23:00:08 +0530

    tabs added

--- frontend/src/components/HomePage/index.tsx
+++ frontend/src/components/HomePage/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, FormControl, FormLabel, Input, Text } from '@chakra-ui/react'
+import { Box, Button, Container, FormControl, FormLabel, Input, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react'
 import React from 'react'
 
 function HomePage() {
@@ -10,21 +10,54 @@ function HomePage() {
                         <Text fontSize='32px' fontWeight={600} fontFamily={'Work sans'}>Talkative</Text>
                     </Box>
 
-                    <FormControl isRequired mb={'24px'}>
-                        <Box mb={'24px'}>
-                            <FormLabel>Username</FormLabel>
-                            <Input placeholder='First name' bg={'white'} />
-                        </Box>
+                    <Tabs variant='soft-rounded' colorScheme='green'>
+                        <TabList>
+                            <Tab>Login</Tab>
+                            <Tab>Sing Up</Tab>
+                        </TabList>
+                        <TabPanels>
+                            <TabPanel>
+                                <div>
+                                    <FormControl isRequired mb={'24px'}>
+                                        <Box mb={'24px'}>
+                                            <FormLabel>Username</FormLabel>
+                                            <Input placeholder='First name' bg={'white'} focusBorderColor='none' outline={'none'} />
+                                        </Box>
 
-                        <Box mb={'24px'}>
-                            <FormLabel>Password</FormLabel>
-                            <Input placeholder='First name' bg={'white'} />
-                        </Box>
+                                        <Box mb={'24px'}>
+                                            <FormLabel>Password</FormLabel>
+                                            <Input placeholder='First name' bg={'white'} />
+                                        </Box>
 
-                        <Box mb={'24px'}>
-                            <Button bg='black' width={'107px'} height={'44px'} textAlign={'center'} lineHeight={'20px'} rounded={'12px'} textColor={'white'}>Button</Button>
-                        </Box>
-                    </FormControl>
+                                        <Box mb={'24px'}>
+                                            <Button bg='black' width={'107px'} height={'44px'} textAlign={'center'} lineHeight={'20px'} rounded={'12px'} textColor={'white'}>Button</Button>
+                                        </Box>
+                                    </FormControl>
+                                </div>
+                            </TabPanel>
+                            <TabPanel>
+                                <div>
+                                    <FormControl isRequired mb={'24px'}>
+                                        <Box mb={'24px'}>
+                                            <FormLabel>Username</FormLabel>
+                                            <Input placeholder='First name' bg={'white'} focusBorderColor='none' outline={'none'} />
+                                        </Box>
+
+                                        <Box mb={'24px'}>
+                                            <FormLabel>Password</FormLabel>
+                                            <Input placeholder='First name' bg={'white'} />
+                                        </Box>
+
+                                        <Box mb={'24px'}>
+                                            <Button bg='black' width={'107px'} height={'44px'} textAlign={'center'} lineHeight={'20px'} rounded={'12px'} textColor={'white'}>Button</Button>
+                                        </Box>
+                                    </FormControl>
+                                </div>
+                            </TabPanel>
+                        </TabPanels>
+                    </Tabs>
+
+
                 </Container>
             </div>
         </>
